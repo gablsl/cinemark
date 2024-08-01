7f12e74109ab49d514213cb2183f7c47eec37442
gablsl <gabrielsousadeveloper@gmail.com>
2024-08-01 18:26:35 -0300

    feat: adding react query to project

--- src/libs/Providers.tsx
+++ src/libs/Providers.tsx
@@ -2,13 +2,18 @@
 
 import { GlobalStyles } from '@/styles/global';
 import { theme } from '@/styles/theme/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 
 export const Providers = (props: React.PropsWithChildren) => {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {props.children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {props.children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
